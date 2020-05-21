db20b2e792f4de914baee7a036f284fa9b824a51
sruthi2001 <64740115+sruthi2001@users.noreply.github.com>
2020-05-21 12:06:44 +0530

    Created size.c
    code for finding the size of char,int,float, double and for  performing multiplication

---
+++ size.c
@@ -0,0 +1,23 @@
+#include<iostream>//1st Question
+using namespace std;
+int main()
+{
+cout << "Size of char: " << sizeof(char) << "byte" << endl;
+cout << "Size of int: " << sizeof(int) << "byte" << endl;
+cout << "Size of float : " << sizeof(float) << "byte" << endl;
+cout << "Size of double: " << sizeof(double) << "byte" << endl;
+reeturn 0;
+}
+
+#include<iostream>//2nd question
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter two numbers: ";
+cin>>a;
+cin>>a;
+c=a*b;
+cout << "\nProduct :"<<c;
+return 0;
+}
